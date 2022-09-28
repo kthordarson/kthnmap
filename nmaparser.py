0e6abca789e5ddf1f4584852704eeb612dbd375a
kthordarson <kthordarson@gmail.com>
2022-09-28 10:18:50 +0200

    configfile

--- nmaparser.py
+++ nmaparser.py
@@ -13,15 +13,22 @@ RELEASE_DATE = "2022-09-20"
 
 def main():
 	parser = OptionParser(usage="%prog [options] --file xmlfile")
-	parser.add_option("-f","--filename", dest="xmlfilename", help="xmlfilename")
+	parser.add_option("-f","--filename", dest="xmlfilename", help="xmlfilename", action='store', type='string')
 	parser.add_option("-p","--path", dest="xmlpath", help="path to xml files")
 	parser.add_option("-s","--session", dest="sessionname", help="sessionname", default="default")
 	parser.add_option("-d","--drop", dest="droptables", help="drop existing data", action="store", default=False)
 	parser.add_option("-c","--check", dest="check", help="check existing data", action="store", default=True)
+	parser.add_option("-r","--read", dest="readconfig", help="run scan from config", action="store", type='string')
 	(options, args) = parser.parse_args()
+	if options.xmlfilename and options.xmlpath:
+		parser.error("Please specify either a filename or a path, not both")
+		return
 	if options.droptables:
 		drop_tables()
 	# Parse nmap file
+	elif options.readconfig:
+		# todo read config file, run nmap scan, parse results, pass results to parser and send to database
+		pass
 	elif options.xmlfilename:
 		scan = nmap.NmapScan(options.xmlfilename)
 		logger.info(f"Total hosts {len(scan.Hosts)} date:{scan.scanstart_str}")
